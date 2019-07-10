30c35b6dbd7751a19e4f0f8edd3f25aa75c8c6a7
Jesper Lloyd <jplloyd@users.noreply.github.com>
2019-07-10 08:44:14 +0200

    appveyor test shell script

---
+++ appveyor.sh
@@ -0,0 +1,9 @@
+echo "This is printed from bash"
+echo "-------------------------------"
+echo "Received these parameters"
+echo "$@"
+echo "-------------------------------"
+bash --version
+echo "-------------------------------"
+pacman --version
+echo "-------------------------------"
