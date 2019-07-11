24126fac7cca0629216074ee2b58e523f1b9619c
Jesper Lloyd <jpl.lloyd@gmail.com>
2019-07-11 12:16:17 +0200

    Add m4 paths via pacman

--- appveyor.sh
+++ appveyor.sh
@@ -22,5 +22,14 @@ pacman --noconfirm -S --needed \
        base-devel \
        ${PKG_PREFIX}-json-c \
        ${PKG_PREFIX}-glib2 \
-       ${PKG_PREFIX}-gobject-introspection &&
+       ${PKG_PREFIX}-gobject-introspection
+
+
+# Make sure that we have the macro files available
+for p in `pacman --noconfirm -Ql ${PKG_PREFIX}-glib2 | grep "\.m4" | xargs readlink -e | xargs dirname | sort --unique`
+do
+    ACLOCAL_PATH=$p:$ACLOCAL_PATH
+done
+export ACLOCAL_PATH
+
 ./autogen.sh && ./configure && make distcheck
